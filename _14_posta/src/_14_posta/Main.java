08c02add917cec5bdec5934072ad8990c8e5be25
Maddalena La Rosa <madda@MaddaBook.local>
2025-06-30 10:26:43 +0200

    ex14 added

---
+++ _14_posta/src/_14_posta/Main.java
@@ -0,0 +1,28 @@
+package _14_posta;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PostOffice posta = new PostOffice();
+		// Aggiungi almeno 3 persone alla coda
+		posta.joinQueue(new Person("Romolo"));
+		posta.joinQueue(new Person("Gianni"));
+		posta.joinQueue(new Person("Gino"));
+		
+		// Mostra la coda
+		posta.showQueue();
+
+		// Visualizza il prossimo da servire
+		posta.whosNext();
+		
+		// Servi due persone
+		posta.serveNext();
+		posta.serveNext();
+		
+		// Mostra la coda rimanente
+		posta.showQueue();
+
+
+	}
+
+}
---
+++ _14_posta/src/_14_posta/PostOffice.java
@@ -0,0 +1,35 @@
+package _14_posta;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PostOffice {
+	private Queue<Person> queue;
+	
+
+    public PostOffice() {
+        queue = new LinkedList<>();
+    }
+    
+    public void joinQueue(Person person) {
+    	queue.add(person);
+        System.out.println(person.getName() + " is in queue.");
+    }
+    
+    public void whosNext() {
+    	Person nextPerson = queue.peek();
+        System.out.println(nextPerson.getName() + " is next.");
+    }
+    
+    public void serveNext() {
+    	Person nextPerson = queue.poll();
+        System.out.println(nextPerson.getName() + " has been served.");
+    }
+    
+    public void showQueue() {
+        System.out.println("Queue:");
+    	for (Person person: queue) {
+    		System.out.println("- " + person.getName());
+    	}
+    }
+  
+}
